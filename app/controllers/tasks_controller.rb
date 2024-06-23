eec012c1a226eb2e0e6dbe80b25f977dbba5573f
eri <k.erii0341115@gmail.com>
2024-06-23 17:37:23 +0900

    rspec_update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,8 +2,8 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
-    @tasks = Task.order(created_at: :desc)
-    @tasks =Task.page(params[:page]).per(10)
+    @tasks = Task.order(created_at: :desc).page(params[:page]).per(10)
+    #@tasks =Task.page(params[:page]).per(10)
     #created_at 列（タスクが作成された日時を表す列）を基準に降順（DESC）で並べ替えるメソッド
   end
 
