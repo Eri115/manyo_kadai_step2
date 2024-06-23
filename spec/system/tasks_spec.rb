9e7197fa4f20db6c2c054b8151f07507d02bede2
eri <k.erii0341115@gmail.com>
2024-06-23 10:40:30 +0900

    rspec_test

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -13,23 +13,33 @@ RSpec.describe 'タスク管理機能', type: :system do
   end
 
   describe '一覧表示機能' do
+    let!(:first_task) { FactoryBot.create(:task, title: 'first_task', created_at: 3.days.ago) }
+    let!(:second_task) { FactoryBot.create(:task, title: 'second_task', created_at: 2.days.ago) }
+    let!(:third_task) { FactoryBot.create(:task, title: 'third_task', created_at: 1.day.ago) }
+   
+  
+    before do
+      visit tasks_path
+    end
+  
     context '一覧画面に遷移した場合' do
       it '登録済みのタスク一覧が作成日時の降順で表示される' do
-        #Task.create!(title:'登録１',content: 'タスク登録完了')
-        #Task.create!(title:'登録２',content: 'タスク登録完了')
-        FactoryBot.create(:task)
-        visit tasks_path
-
-
-        expect(page).to have_content('企画の予算案を作成する。')
+        task_titles = all('.task-title td:first-child').map(&:text)
+        expect(task_titles).to eq ['third_task', 'second_task', 'first_task']
       end
     end
-    centext '新たにタスクを作成した場合' do
+  
+    context '新たにタスクを作成した場合' do
       it '新しいタスクが一番上に表示される' do
-
+        FactoryBot.create(:task, title: 'new_task', content: '新しいタスクの内容', created_at: Time.now)
+        visit tasks_path
+    
+        task_titles = all('.task-title').map{ |element| element.find('td:first-child').text.strip }
+        expect(task_titles.first).to eq 'new_task'
       end
     end
   end
+
 
   describe '詳細表示機能' do
      context '任意のタスク詳細画面に遷移した場合' do
