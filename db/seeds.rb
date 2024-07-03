ea1f3ca3f049cf4782d14d5d4176b1a4b01365a6
eri <k.erii0341115@gmail.com>
2024-07-03 15:59:53 +0900

    add:seedファイル修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,31 +6,47 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-#Task.create!(title: "first_task", content: "1")
-
 #5.times do |i|
-  #Task.create(title: "first_task#{i+1}", content: "Content#{i+1}",deadline_on:"2024-2-18", priority: :medium, status: :waiting)
+#  Task.create!(
+#    title: "second_task#{i+1}",
+#    content: "Content#{i+1}",
+#    deadline_on: "2024-02-19",
+#    priority: :high,
+#   status: :working  )
 #end
 
 #5.times do |i|
-  #Task.create(title: "second_task#{i+1}", content: "Content#{i+1}",deadline_on:"2024-2-19", priority: :high, status: :working)
+#  Task.create!(
+#    title: "third_task#{i+1}",
+#    content: "Content#{i+1}",
+#    deadline_on: "2024-02-20",
+#    priority: :low,
+#    status: :completed
+#  )
 #end
 
 #5.times do |i|
-  #Task.create(title: "third_task#{i+1}", content: "Content#{i+1}",deadline_on:"2024-2-20", priority: :low, status: :completed)
+  #Task.create!(
+   #title: "third_task#{i+1}",
+    #content: "Content#{i+1}",
+   #deadline_on: "2024-02-20",
+#    priority: :low,
+#    status: :completed
+#  )
 #end
 
-user = FactoryBot.create(:user, email: 'user@example.com')
 
-# 管理者の作成
-admin = FactoryBot.create(:admin_user, email: 'admin@example.com')
-
-# 一般ユーザーに関連付けられたタスクの生成
-50.times do
-  FactoryBot.create(:task, user: user)
+date_list = ['2025/10/01', '2025/12/01', '2025/01/01', '2025/05/01', '2025/03/01', '2025/11/01', '2025/02/01',
+             '2025/09/01', '2025/04/01', '2025/01/01']
+priority_list = [2, 0, 2, 1, 1, 0, 1, 2, 1, 2]
+status_list = [0, 2, 2, 0, 1, 1, 0, 2, 1, 2]
+general_user = User.create!(name: '一般ユーザ', email: 'ippan@aaa.aa', password: '123456', admin: false)
+admin_user = User.create!(name: '管理者ユーザ', email: 'kanrisha@aaa.aa', password: '123456', admin: true)
+50.times do |n|
+  Task.create!(title: "task_title_seed#{n + 1}", content: "task_content_seed#{n + 1}", deadline_on: date_list[n / 10],
+               priority: priority_list[n / 10], status: status_list[n / 10], user_id: general_user.id)
 end
-
-# 管理者に関連付けられたタスクの生成
-50.times do
-  FactoryBot.create(:task, user: admin)
+50.times do |n|
+  Task.create!(title: "task_title_seed#{n + 1}", content: "task_content_seed#{n + 1}", deadline_on: date_list[n / 10],
+               priority: priority_list[n / 10], status: status_list[n / 10], user_id: admin_user.id)
 end
