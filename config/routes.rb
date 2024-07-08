473ca9b93c25948eecdc1adaa69c7f490fa515f7
eri <k.erii0341115@gmail.com>
2024-07-08 11:30:11 +0900

    fix:エラーページ修正

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,6 @@ Rails.application.routes.draw do
     resources :users, only: [:index, :new,:show, :create, :edit, :update, :destroy]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  #match "/404", to: "errors#not_found", via: :all
-  #match "/500", to: "errors#internal_server_error", via: :all
+  match "/404", to: "errors#not_found", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
 end
